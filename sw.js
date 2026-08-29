127889eed6a813b0881caf70426b45c2b2200243
jasongalorio99-png <jason.galorio.99@gmail.com>
2026-08-29 07:13:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'bonded-store-v8';
+const CACHE_NAME = 'bonded-store-v9';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './icon-maskable-512.png'
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-512.png'
 ];
 
 self.addEventListener('install', function(event){
